3399beeb41179d6015e6a0dd0cda43b7612d70a7
DiuleJR <syfras999@gmail.com>
2022-03-03 17:07:13 -0300

    a

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 password = "23051401@"  # <---Mude a senha
 
-import requests, random, string, secmail, pyshorteners, amino, names, json
+import requests, random, string, secmail, pyshorteners, amino, names, json, os
 from bs4 import BeautifulSoup
 from time import sleep
 from amino.lib.util.exceptions import ActionNotAllowed, IncorrectVerificationCode, ServiceUnderMaintenance
 from pyfiglet import figlet_format
 from urllib.request import urlopen
+from flask import Flask
 
 abertura = figlet_format("a c c g e n  X\n       p t - b r")
 print(abertura)
@@ -63,80 +64,92 @@ def salvar(data):
 
 # ==================Gerador=============================
 print("[\033[1;31mAtenção\033[m] \033[1;33mVocê pode criar somente 5 contas por VPN\033[m")
-contador = 0
 
-while True:
 
-    try:
-        with open("device.json", "w") as f:
-            f.close()
+app = Flask(__name__)
 
-        if contador == 5:
-            print("\n[\033[1;31mAtenção\033[m] \033[1;33mVocê criou 5 contas, mude o VPN!")
-            contador = 0
+@app.route('/', methods=["POST"])
+def home():
+    while True:
+        contador = 0
+        try:
+            with open("device.json", "w") as f:
+                f.close()
+
+            if contador == 5:
+                print("\n[\033[1;31mAtenção\033[m] \033[1;33mVocê criou 5 contas, mude o VPN!")
+                contador = 0
+                break
+
+            client = amino.Client()
+            email = gerar_email()
+            nickname = nome_aleatorio() + '⁹⁹⁹'
+            print(f"\n[\033[1;31mGerando email\033[m][\033[1;35m{email}\033[m][\033[1;32m{contador + 1}\033[m]")
+            client.request_verify_code(email=email)
+            link = encurtar_link(link_codigo(email))
+            print(f"[ \033[1;33mLink\033[m ] \033[1;32m{link}\033[m")
+            codigo = api(link)
+
+            print(f"[\033[1;37mCódigo\033[m]: {codigo}")
+            # codigo = input("[\033[1;37mCódigo\033[m]: ")
+            # slk = api(link)
+            if codigo == '':
+                print("\n[\033[1;31mAtenção\033[m] \033[1;33mVocê não digitou o código, reinicie o script!")
+                break
+
+            device = deviceId()
+            client.register(nickname, email, password, codigo, device)
+            client.login(email=email, password=password)
+            # client.join_community("39276113")  # <----- Sua cid da comunidade
+            #img = urlopen(f"{link}").read()
+            #open(f"G:/AMINO COINS/FOLLOW/bd_captcha/{codigo}.png", "wb").write(img)
+
+            contador += 1
+
+            d = {
+                "email": str(email),
+                "password": str(password),
+                "device": str(device)
+                }
+
+
+
+            j = json.dumps(d)
+            data = {'data': j}
+            salvar(data)
+            print("[\033[1;32mConta salva!\033[m]")
+
+
+        except ActionNotAllowed:
+            print("\n[\033[1;31mAtenção\033[m] \033[1;33mLimite de contas criadas atingido, mude o VPN!\033[m")
             break
 
-        client = amino.Client()
-        email = gerar_email()
-        nickname = nome_aleatorio() + '⁹⁹⁹'
-        print(f"\n[\033[1;31mGerando email\033[m][\033[1;35m{email}\033[m][\033[1;32m{contador + 1}\033[m]")
-        client.request_verify_code(email=email)
-        link = encurtar_link(link_codigo(email))
-        print(f"[ \033[1;33mLink\033[m ] \033[1;32m{link}\033[m")
-        codigo = api(link)
-
-        print(f"[\033[1;37mCódigo\033[m]: {codigo}")
-        # codigo = input("[\033[1;37mCódigo\033[m]: ")
-        # slk = api(link)
-        if codigo == '':
-            print("\n[\033[1;31mAtenção\033[m] \033[1;33mVocê não digitou o código, reinicie o script!")
+        except IncorrectVerificationCode:
+            print("\n[\033[1;31mAtenção\033[m] \033[1;33mVocê digitou o código errado, reinicie o script!\033[m")
             break
 
-        device = deviceId()
-        client.register(nickname, email, password, codigo, device)
-        client.login(email=email, password=password)
-        # client.join_community("39276113")  # <----- Sua cid da comunidade
-        #img = urlopen(f"{link}").read()
-        #open(f"G:/AMINO COINS/FOLLOW/bd_captcha/{codigo}.png", "wb").write(img)
+        except ServiceUnderMaintenance:
+            print("\n[\033[1;31mAtenção\033[m] \033[1;33mParece que o serviço está em manutenção, tente mais tarde!")
+            break
 
-        contador += 1
-
-        d = {
-            "email": str(email),
-            "password": str(password),
-            "device": str(device)
-            }
+        """except:
+            print("\n[\033[1;31mAtenção\033[m] \033[1;33mErro desconhecido, tente reiniciar o script!")
+            break"""
 
 
+        """with open("accounts.json", "a+") as x:
+            acc = f'\n{{\n"email": "{email}",\n"password": "{password}",\n"device": "{device}"\n}},'
+            # acc = f'\n{{\n"email": "{email}",\n"password": "{password}",\n"device": "{device}"\n}},'
+            x.write(acc)
+    
+        with open("emails.txt", "a+") as c:
+            acc = f"{email}\n"
+            c.write(acc)"""
 
-        j = json.dumps(d)
-        data = {'data': j}
-        salvar(data)
-        print("[\033[1;32mConta salva!\033[m]")
-
-
-    except ActionNotAllowed:
-        print("\n[\033[1;31mAtenção\033[m] \033[1;33mLimite de contas criadas atingido, mude o VPN!\033[m")
-        break
-
-    except IncorrectVerificationCode:
-        print("\n[\033[1;31mAtenção\033[m] \033[1;33mVocê digitou o código errado, reinicie o script!\033[m")
-        break
-
-    except ServiceUnderMaintenance:
-        print("\n[\033[1;31mAtenção\033[m] \033[1;33mParece que o serviço está em manutenção, tente mais tarde!")
-        break
-
-    """except:
-        print("\n[\033[1;31mAtenção\033[m] \033[1;33mErro desconhecido, tente reiniciar o script!")
-        break"""
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
-    """with open("accounts.json", "a+") as x:
-        acc = f'\n{{\n"email": "{email}",\n"password": "{password}",\n"device": "{device}"\n}},'
-        # acc = f'\n{{\n"email": "{email}",\n"password": "{password}",\n"device": "{device}"\n}},'
-        x.write(acc)
-
-    with open("emails.txt", "a+") as c:
-        acc = f"{email}\n"
-        c.write(acc)"""
+if __name__ == "__main__":
+    home()
